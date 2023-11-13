c018adbcb4d81f377d0fe783ea27ae01566e2b8a
Andrei Virtosu <andrei.virtosu@gmail.com>
2023-11-13 09:50:01 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,15 +2,18 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ','
+vim.g.context_enabled=0
 
 vim.opt.rnu = true
+vim.opt.nu = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
+vim.g.nowrap = true
 vim.opt.wrap = true
 vim.opt.breakindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
 vim.opt.updatetime = 300
@@ -19,6 +22,12 @@ vim.o.cursorline = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.swapfile = false
 vim.opt.scrolloff = 10 -- Make it so there are always ten lines below my cursor
+
+
+-- vim.opt.foldlevel = 20
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr"
+
 
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 local install_plugins = false
@@ -32,6 +41,17 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.cmd('packadd packer.nvim')
   install_plugins = true
 end
+
+require'packer'.init{
+    git = {
+      subcommands = {
+        -- Fix for https://github.com/wbthomason/packer.nvim/issues/1100,
+        -- which blocks updating the nvim-tree nightly tag
+        update = 'pull --ff-only --progress --rebase=false --force',
+        fetch = 'fetch --depth 999999 --progress --force',
+      }
+    },
+}
 
 require('packer').startup(function(use)
 
@@ -64,7 +84,7 @@ require('packer').startup(function(use)
     -- or                            , branch = '0.1.x',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use { "williamboman/mason.nvim" }
+  -- use { "williamboman/mason.nvim" }
   use { "neoclide/coc.nvim", branch = 'release'}
   use {
     'nvim-treesitter/nvim-treesitter',
@@ -87,13 +107,17 @@ require('packer').startup(function(use)
       require('windows').setup()
     end
   }
-  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+  -- use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
   use {
     'rmagatti/auto-session',
     config = function()
+      -- local function restore_nvim_tree()
+      --   require("nvim-tree.api").tree.open({focus=false})
+      -- end
       require("auto-session").setup {
         log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+        -- post_restore_cmds = {restore_nvim_tree}
       }
     end
   }
@@ -113,29 +137,10 @@ require('packer').startup(function(use)
   --    require('pretty-fold').setup()
   -- end
   --}
-  use {
-    'jedrzejboczar/toggletasks.nvim',
-    requires = {
-        'nvim-lua/plenary.nvim',
-        'akinsho/toggleterm.nvim',
---        'nvim-telescope/telescope.nvim/',
-    },
-    -- To enable YAML config support
-    rocks = 'lyaml',
-  }
   --use {
   --  'stevearc/overseer.nvim',
   --  config = function() require('overseer').setup() end
   --}
-  --use {
-  --'EthanJWright/vs-tasks.nvim',
-  --  requires = {
-  --    'nvim-lua/popup.nvim',
-  --    'nvim-lua/plenary.nvim',
-  --    'nvim-telescope/telescope.nvim'
-  --  }
-  --}
-
   use {
     "windwp/nvim-autopairs"
     , config = function() require("nvim-autopairs").setup {} end
@@ -187,61 +192,7 @@ require('packer').startup(function(use)
   }
   use { "nvim-telescope/telescope-file-browser.nvim" }
 
-  use({
-    "jackMort/ChatGPT.nvim",
-    config = function()
-      require("chatgpt").setup({
-        -- optional configuration
-      })
-    end,
-    requires = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
-  })
-
-  -- use({
-  --   "epwalsh/obsidian.nvim",
-  --   config = function()
-  --     require("obsidian").setup({
-  --       use_advanced_uri = true,
-  --       dir = "~/vaults/dev/",
-  --       daily_notes = {
-  --         folder = "dailies",
-  --       },
-  --       -- completion = {
-  --       --   nvim_cmp = true,
-  --       -- },
-  --       note_id_func = function(title)
-  --         local sane_name = ""
-  --         if title ~= nil then
-  --           -- If title is given, transform it into valid file name.
-  --           sane_name = title:gsub(" ", "_"):gsub("[^A-Za-z0-9-]", ""):lower()
-  --         else
-  --           -- If title is nil, just add 4 random uppercase letters to the suffix.
-  --           for _ in 1, 4 do
-  --             sane_name = sane_name .. string.char(math.random(65, 90))
-  --           end
-  --         end
-  --         return sane_name
-  --       end,
-  --     })
-  --   end,
-  -- })
-
   use { "ThePrimeagen/harpoon" }
-  use {'lervag/vimtex',
-    config = function() 
-      vim.cmd([[
-        let g:vimtex_view_method='zathura'
-        " let g:vimtex_view_general_viewer='zathura'
-        " let g:vimtex_view_enabled=1
-        " let g:vimtex_compiler_method='latexrun'
-        " let g:vimtex_quickfix_mode=0
-      ]])
-    end
-  }
   use({
     "iamcco/markdown-preview.nvim",
     run = function() vim.fn["mkdp#util#install"]() end,
@@ -260,6 +211,38 @@ require('packer').startup(function(use)
     end
   }
 
+  use {
+    "wellle/context.vim" 
+  }
+
+  use {
+    'chentoast/marks.nvim'
+  }
+
+  use {'arzg/vim-colors-xcode'}
+
+  --use {'tomasiser/vim-code-dark'}
+  use {'Mofiqul/vscode.nvim'}
+
+  -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
+  use 'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
+  use 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
+  use 'romgrk/barbar.nvim'
+
+  use { 
+    "folke/tokyonight.nvim", 
+    lazy = false, 
+    priority = 1000, 
+    opts = {}, 
+  }
+
+  use { 'rktjmp/lush.nvim'}
+  use { 'metalelf0/jellybeans-nvim' }
+
+  use { 'bfrg/vim-cpp-modern' }
+
+  -- use { 'jackguo380/vim-lsp-cxx-highlight' }
+
   if install_plugins then
     require('packer').sync()
   end
@@ -270,47 +253,53 @@ if install_plugins then
   return
 end
 
--- vim.g.vimtex_view_method = 'zathura'
-
-
 --vim.cmd('colorscheme habamax')
 --vim.cmd('colorscheme rose-pine')
 --require('rose-pine').setup({
 --  dark_variant = 'moon'
 --})
-vim.cmd('colorscheme duskfox')
+-- vim.cmd('colorscheme duskfox')
+-- vim.cmd('colorscheme vscode')
+-- vim.cmd('colorscheme dayfox')
+vim.cmd('colorscheme jellybeans-nvim')
 
-require("bufferline").setup{
-  options = {
-    numbers = "both",
-    diagnostics = "coc",
-    truncate_names = false,
-    -- rest of config ...
-
-    --- count is an integer representing total count of errors
-    --- level is a string "error" | "warning"
-    --- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
-    --- this should return a string
-    --- Don't get too fancy as this function will be executed a lot
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local icon = level:match("error") and " " or " "
-      return " " .. icon .. count
-    end
-
-  }
-}
+-- require("bufferline").setup{
+--   options = {
+--     numbers = "both",
+--     diagnostics = "coc",
+--     truncate_names = false,
+--     -- rest of config ...
+--
+--     --- count is an integer representing total count of errors
+--     --- level is a string "error" | "warning"
+--     --- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
+--     --- this should return a string
+--     --- Don't get too fancy as this function will be executed a lot
+--     diagnostics_indicator = function(count, level, diagnostics_dict, context)
+--       local icon = level:match("error") and " " or " "
+--       return " " .. icon .. count
+--     end
+--
+--   }
+-- }
 
 require('lualine').setup({
   options = {
     icons_enabled = false,
     section_separators = '',
-    component_separators = ''
+    component_separators = '',
+    theme = 'vscode'
   }
 })
 
 require("nvim-tree").setup({
-  open_on_setup = true,
-  open_on_tab = true,
+  -- open_on_setup = false,
+  -- open_on_setup_file = false,
+  open_on_tab = false,
+  filters = {
+          custom= { '00', '10', '11', '12', '15', '16', '17', '20', '21', '22', '30', '31', '32', '90', '91'}
+
+  },
   view = {
     adaptive_size = false,
     relativenumber = true
@@ -341,24 +330,7 @@ require("nvim-tree").setup({
 })
 
 require("toggleterm").setup{}
-require("mason").setup()
 require("gitsigns").setup()
--- require("refactoring").setup({
---   prompt_func_return_type = {
---     cpp = true,
---     c = true,
---     h = true,
---     hpp = true,
---     cxx = true
---   },
---   prompt_func_param_type = {
---     cpp = true,
---     c = true,
---     h = true,
---     hpp = true,
---     cxx = true
---   },
--- })
 
  require'nvim-treesitter.configs'.setup {
    highlight = {
@@ -368,9 +340,6 @@ require("gitsigns").setup()
  }
 
 
--- require('toggletasks').setup({
--- })
-require('telescope').load_extension('toggletasks')
 require('telescope').load_extension('neoclip')
 --require('telescope').load_extension('file_browser')
 require('telescope').load_extension('harpoon')
@@ -378,28 +347,70 @@ require('telescope').load_extension('harpoon')
 require('Comment').setup({
 })
 
+require("auto-session").setup {
+  bypass_session_save_file_types = nil, -- table: Bypass auto save when only buffer open is one of these file types
+  cwd_change_handling = { -- table: Config for handling the DirChangePre and DirChanged autocmds, can be set to nil to disable altogether
+    restore_upcoming_session = true, -- boolean: restore session for upcoming cwd on cwd change
+    pre_cwd_changed_hook = nil, -- function: This is called after auto_session code runs for the `DirChangedPre` autocmd
+    post_cwd_changed_hook = nil, -- function: This is called after auto_session code runs for the `DirChanged` autocmd
+  },
+}
 
+require'marks'.setup {
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  default_mappings = true,
+  -- builtin_marks = {",", "<", ">", "^"},
+  builtin_marks = {},
+  cyclic = true,
+  bookmark_0 = {
+    sign = "⚑",
+    virt_text = "hello world",
+    -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
+    -- defaults to false.
+    annotate = false,
+  },
+  mappings = {}
 
--- local remap = vim.api.nvim_set_keymap
--- local npairs = require('nvim-autopairs')
--- npairs.setup({map_cr=false})
+}
+
+-- local function open_nvim_tree()
 --
--- -- skip it, if you use another global object
--- _G.MUtils= {}
---
--- MUtils.completion_confirm=function()
---   if vim.fn.pumvisible() ~= 0  then
---     --return vim.fn[[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]]
---     --return vim.fn["coc#pum#confirm"]()
---     vim.fn["coc#_select_confirm"]()
---   else
---     return npairs.autopairs_cr()
---   end
+--   -- open the tree
+--   require("nvim-tree.api").tree.open()
 -- end
---remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
---------------------
+-- local function open_nvim_tree(data)
+--
+--   -- buffer is a real file on the disk
+--   local real_file = vim.fn.filereadable(data.file) == 1
+--
+--   -- buffer is a [No Name]
+--   local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+--
+--   if not real_file and not no_name then
+--     return
+--   end
+--
+--   -- open the tree, find the file but don't focus it
+--   require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+-- end
+
+-- local function open_nvim_tree(data)
+--
+--   -- buffer is a directory
+--   local directory = vim.fn.isdirectory(data.file) == 1
+--
+--   if not directory then
+--     return
+--   end
+--
+--   -- change to the directory
+--   vim.cmd.cd(data.file)
+--
+--   -- open the tree
+--   require("nvim-tree.api").tree.open()
+-- end
+--vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
@@ -417,13 +428,18 @@ end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
 vim.keymap.set({'n', 't'},'<leader>t', '<cmd>1ToggleTerm size=20 direction=float <cr>')
+vim.keymap.set({'n', 't'},'<leader>h', '<cmd>2ToggleTerm size=13 direction=horizontal <cr>')
 --vim.keymap.set({'n', 't'},'<leader>z', '<cmd>4ToggleTerm size=10 direction=float <cr>')
+
+vim.keymap.set({'n', 't'},'<leader>m', '<cmd>2TermExec cmd="cd /home/van/projects/na-dec/dec && make -j12" <cr>')
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
+vim.keymap.set({'n', 't'},'<leader>fw', '<cmd>Telescope grep_string <cr>')
 
 local keyset = vim.keymap.set
 -- Auto complete
@@ -461,23 +477,24 @@ keyset('n', '<C-w>=', cmd 'WindowsEqualize')
 keyset('n', '<C-w>=', cmd 'WindowsEqualize')
 
 --nnoremap <silent> gb :BufferLinePick<CR>,
-keyset("n", "<leader>bb", cmd 'BufferLinePick' )
-keyset("n", "<leader>bd", cmd 'BufferLinePickClose' )
-keyset("n", "<C-k>", cmd 'BufferLineCycleNext' )
-keyset("n", "<C-j>", cmd 'BufferLineCyclePrev' )
+-- keyset("n", "<leader>bb", cmd 'BufferLinePick' )
+-- keyset("n", "<leader>bd", cmd 'BufferLinePickClose' )
+-- keyset("n", "<C-k>", cmd 'BufferLineCycleNext' )
+-- keyset("n", "<C-j>", cmd 'BufferLineCyclePrev' )
+keyset("n", "<leader>gg", cmd 'LazyGit')
 
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions,globals"
 
-keyset('n', '<leader>1', cmd 'BufferLineGoToBuffer 1') 
-keyset('n', '<leader>2', cmd 'BufferLineGoToBuffer 2') 
-keyset('n', '<leader>3', cmd 'BufferLineGoToBuffer 3') 
-keyset('n', '<leader>4', cmd 'BufferLineGoToBuffer 4') 
-keyset('n', '<leader>5', cmd 'BufferLineGoToBuffer 5') 
-keyset('n', '<leader>6', cmd 'BufferLineGoToBuffer 6') 
-keyset('n', '<leader>7', cmd 'BufferLineGoToBuffer 7') 
-keyset('n', '<leader>8', cmd 'BufferLineGoToBuffer 8') 
-keyset('n', '<leader>9', cmd 'BufferLineGoToBuffer 9') 
-keyset('n', '<leader>$', cmd 'BufferLineGoToBuffer $') 
+-- keyset('n', '<leader>1', cmd 'BufferLineGoToBuffer 1') 
+-- keyset('n', '<leader>2', cmd 'BufferLineGoToBuffer 2') 
+-- keyset('n', '<leader>3', cmd 'BufferLineGoToBuffer 3') 
+-- keyset('n', '<leader>4', cmd 'BufferLineGoToBuffer 4') 
+-- keyset('n', '<leader>5', cmd 'BufferLineGoToBuffer 5') 
+-- keyset('n', '<leader>6', cmd 'BufferLineGoToBuffer 6') 
+-- keyset('n', '<leader>7', cmd 'BufferLineGoToBuffer 7') 
+-- keyset('n', '<leader>8', cmd 'BufferLineGoToBuffer 8') 
+-- keyset('n', '<leader>9', cmd 'BufferLineGoToBuffer 9') 
+-- keyset('n', '<leader>$', cmd 'BufferLineGoToBuffer $') 
 
 keyset('n', '<F8>', cmd 'TagbarToggle') 
 keyset('n', '<M-o>', cmd 'CocCommand clangd.switchSourceHeader') 
@@ -487,7 +504,7 @@ keyset('n', '<M-o>', cmd 'CocCommand clangd.switchSourceHeader')
 
 keyset("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
 -- Formatting selected code.
-keyset({"x","n"}, "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
+keyset({"x","n"}, "<leader>k", "<Plug>(coc-format-selected)", {silent = true})
 
 -- Apply AutoFix to problem on the current line.
 keyset("n", "<leader>qf", "<Plug>(coc-fix-current)", {silent = true, nowait = true})
@@ -524,11 +541,10 @@ keyset("n", "<leader>n", cmd "NvimTreeFindFile!")
 keyset("n", "<leader>v", cmd "NvimTreeToggle")
 
 
-
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 -- vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
@@ -559,4 +575,52 @@ vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 --
 -- nvim_create_augroups(autocmds)
 
+-- Commands
 --vim.cmd('Gitsigns toggle_current_line_blame')
+
+-- vim.api.nvim_create_autocmd({ "FileType c,cpp set local equalprg=clang-format" }, { callback = open_nvim_tree })
+-- vim.api.nvim_command("set equalprg=clang-format")
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<C-j>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<C-k>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<C-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<C->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<leader>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<leader>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<leader>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<leader>5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<leader>6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<leader>7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<leader>8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<leader>9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<leader>0', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- Sort automatically by...
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
+
+-- Other:
+-- :BarbarEnable - enables barbar (enabled by default)
+-- :BarbarDisable - very bad command, should never be used
